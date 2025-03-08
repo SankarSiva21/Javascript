0331b8505e551ad240b131d33b8a56f2bb68ee30
SivaSankar <janaganisiva23@gmail.com>
2025-03-08 09:24:05 +0530

    added typeof

--- Variable.js
+++ Variable.js
@@ -10,3 +10,13 @@ let num3 = 10;
 console.log(num1);
 console.log(num2);
 console.log(num3);
+
+const number = 10;
+const String = 'Sivasankar';
+const bool = false;
+const object = {key:"value",key2:"value2"};
+
+console.log(typeof number) ;
+console.log(typeof String) ;
+console.log(typeof bool);
+console.log(typeof object); 
