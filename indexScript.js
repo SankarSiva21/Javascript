30491070e4627d953582b685fb7c10ad3a55d63c
SivaSankar <janaganisiva23@gmail.com>
2025-03-22 10:15:42 +0530

    function to chnage list item content

--- indexScript.js
+++ indexScript.js
@@ -10,5 +10,19 @@ function addParagraph() {
 }
 
 // btn.addEventListener('clcck', addParagraph);
-btn.onclick = addParagraph; 
+btn.onclick = addParagraph;
 
+const list = document.querySelector('ol'); // selects the ol in the page 
+list.nodeType; // gives the node type/reference number https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
+
+const listItem = list.children.item(1); // selects the child elemt 
+// listItem.textContent = "New constnt"; // will change the content of the of sn element 
+
+const listButton = document.querySelector('#list-item');
+
+function changeListContent() {
+    const list = document.querySelector('ol');
+    const listItem = list.children.item(2);
+    listItem.textContent = "New Content";
+}
+listButton.onclick = changeListContent;
